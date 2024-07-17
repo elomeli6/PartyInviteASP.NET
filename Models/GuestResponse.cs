0b9ab9051cb3d92eb1fbf75ef76d7d3ebd6ba256
Eric Lomeli-Gil <elomeligil@Erics-MacBook-Air.local>
2024-07-17 16:57:28 -0700

    added validations to GuestResponse Objects to ensure user enter required data

--- Models/GuestResponse.cs
+++ Models/GuestResponse.cs
@@ -4,11 +4,17 @@ the real-world objects, processes, and rules that define the application also ca
 make up the universe of the application and the methods to manipulate them.
 */
 
+using System.ComponentModel.DataAnnotations; //This is the libray/namespace that allows to access validation rules known as attributes that .NET uses to validate data during the Model Binding Process
 namespace PartyInvites.Models {
     public class GuestResponse {
+
+        [Required(ErrorMessage = "Please enter your name")]
         public string? Name { get; set; } //All properties are nullable -> ?
+        [Required(ErrorMessage = "Please enter your email address")[EmailAddress]]
         public string? Email { get; set;}
+        [Required(ErrorMessage ="Please enter valid phone number")]
         public string? Phone { get; set;}
+        [Required(ErrorMessage ="Please specify if you will attend")]
         public bool? WillAttend {get; set;}
     }
 }
